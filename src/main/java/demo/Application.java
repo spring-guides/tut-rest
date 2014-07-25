f77f73fbd231b518626143d90602b2560243d81d
Josh Long <josh@joshlong.com>
2014-07-24 22:05:47 -0700

    improved the comment examples.

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -48,7 +48,9 @@ import java.util.*;
 
 //
 // curl -X POST -vu android-bookmarks:123456 http://localhost:8080/oauth/token -H "Accept: application/json" -d "password=password&username=jlong&grant_type=password&scope=write&client_secret=123456&client_id=android-bookmarks"
-// curl -v POST http://127.0.0.1:8080/tags --data "tags=cows,dogs"  -H "Authorization: Bearer 66953496-fc5b-44d0-9210-b0521863ffcb"
+// take the access_token attribute from the response and then replace the access token after 'Bearer'
+// curl -v GET http://127.0.0.1:8080/bookmarks  -H "Authorization: Bearer 9410de0b-3b29-41cb-b85f-2cb52a8d2115"
+  
 
 @Configuration
 @ComponentScan
