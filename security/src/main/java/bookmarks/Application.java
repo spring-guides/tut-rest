828590b42a2b536463850d8f98f7d98ff3ee8827
Dave Syer <dsyer@pivotal.io>
2014-11-03 09:38:36 +0000

    Fix password grant in security sample
    We need to add a workaround for a Spring Boot issue (fixed in
    1.1.9). The AuthenticationManager supplied by Spring Boot gets
    created too early and injected into out OAuth2 server, unless we
    defer it a bit by making it a lazy implementation that initializes
    itself only when used.
    
    Fixes gh-2

--- security/src/main/java/bookmarks/Application.java
+++ security/src/main/java/bookmarks/Application.java
@@ -1,6 +1,14 @@
 package bookmarks;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.io.IOException;
+import java.net.URI;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,12 +18,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.net.URI;
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,6 +38,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -64,195 +68,213 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class Application {
 
-    // CORS
-    @Bean
-    FilterRegistrationBean corsFilter(@Value("${tagit.origin:http://localhost:9000}") String origin) {
-        return new FilterRegistrationBean(new Filter() {
-            public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-                    throws IOException, ServletException {
-                HttpServletRequest request = (HttpServletRequest) req;
-                HttpServletResponse response = (HttpServletResponse) res;
-                String method = request.getMethod();
-                // this origin value could just as easily have come from a database
-                response.setHeader("Access-Control-Allow-Origin", origin);
-                response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
-                response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
-                response.setHeader("Access-Control-Allow-Credentials", "true");
-                response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
-                if ("OPTIONS".equals(method)) {
-                    response.setStatus(HttpStatus.OK.value());
-                } else {
-                    chain.doFilter(req, res);
-                }
-            }
+	// CORS
+	@Bean
+	FilterRegistrationBean corsFilter(
+			@Value("${tagit.origin:http://localhost:9000}") String origin) {
+		return new FilterRegistrationBean(new Filter() {
+			public void doFilter(ServletRequest req, ServletResponse res,
+					FilterChain chain) throws IOException, ServletException {
+				HttpServletRequest request = (HttpServletRequest) req;
+				HttpServletResponse response = (HttpServletResponse) res;
+				String method = request.getMethod();
+				// this origin value could just as easily have come from a database
+				response.setHeader("Access-Control-Allow-Origin", origin);
+				response.setHeader("Access-Control-Allow-Methods",
+						"POST,GET,OPTIONS,DELETE");
+				response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
+				response.setHeader("Access-Control-Allow-Credentials", "true");
+				response.setHeader(
+						"Access-Control-Allow-Headers",
+						"Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+				if ("OPTIONS".equals(method)) {
+					response.setStatus(HttpStatus.OK.value());
+				}
+				else {
+					chain.doFilter(req, res);
+				}
+			}
 
-            public void init(FilterConfig filterConfig) {
-            }
+			public void init(FilterConfig filterConfig) {
+			}
 
-            public void destroy() {
-            }
-        });
-    }
+			public void destroy() {
+			}
+		});
+	}
 
-    @Bean
-    CommandLineRunner init(AccountRepository accountRepository, BookmarkRepository bookmarkRepository) {
-        return (evt) ->
-                Arrays.asList("jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(",")).forEach(a -> {
-                    Account account = accountRepository.save(new Account(a, "password"));
-                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + a, "A description"));
-                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + a, "A description"));
-                });
-    }
+	@Bean
+	CommandLineRunner init(AccountRepository accountRepository,
+			BookmarkRepository bookmarkRepository) {
+		return (evt) -> Arrays.asList(
+				"jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(","))
+				.forEach(
+						a -> {
+							Account account = accountRepository.save(new Account(a,
+									"password"));
+							bookmarkRepository.save(new Bookmark(account,
+									"http://bookmark.com/1/" + a, "A description"));
+							bookmarkRepository.save(new Bookmark(account,
+									"http://bookmark.com/2/" + a, "A description"));
+						});
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
-
 
 @Configuration
 class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
-    @Autowired
-    AccountRepository accountRepository;
+	@Autowired
+	AccountRepository accountRepository;
 
-    @Override
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
-    }
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService());
+	}
 
-    @Bean
-    UserDetailsService userDetailsService() {
-        return (username) ->
-                accountRepository.findByUsername(username)
-                        .map(a -> new User(a.username, a.password, true, true, true, true, AuthorityUtils.createAuthorityList("USER", "write")))
-                        .orElseThrow(() -> new UsernameNotFoundException("could not find the user '" + username + "'"));
-    }
+	@Bean
+	UserDetailsService userDetailsService() {
+		return (username) -> accountRepository
+				.findByUsername(username)
+				.map(a -> new User(a.username, a.password, true, true, true, true,
+						AuthorityUtils.createAuthorityList("USER", "write")))
+				.orElseThrow(
+						() -> new UsernameNotFoundException("could not find the user '"
+								+ username + "'"));
+	}
 }
-
 
 @Configuration
 @EnableResourceServer
 @EnableAuthorizationServer
 class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
 
-    String applicationName = "bookmarks";
+	String applicationName = "bookmarks";
 
-    // This is required for password grants, which we specify below as one of the  {@literal authorizedGrantTypes()}.
-    @Autowired
-    AuthenticationManager authenticationManager;
+	// This is required for password grants, which we specify below as one of the
+	// {@literal authorizedGrantTypes()}.
+	@Autowired
+	AuthenticationManagerBuilder authenticationManager;
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-    }
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+			throws Exception {
+		// Workaround for https://github.com/spring-projects/spring-boot/issues/1801
+		endpoints.authenticationManager(new AuthenticationManager() {
+			@Override
+			public Authentication authenticate(Authentication authentication)
+					throws AuthenticationException {
+				return authenticationManager.getOrBuild().authenticate(authentication);
+			}
+		});
+	}
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-        clients.inMemory()
-                .withClient("android-" + applicationName)
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-                .authorities("ROLE_USER")
-                .scopes("write")
-                .resourceIds(applicationName)
-                .secret("123456");
-    }
+		clients.inMemory().withClient("android-" + applicationName)
+				.authorizedGrantTypes("password", "authorization_code", "refresh_token")
+				.authorities("ROLE_USER").scopes("write").resourceIds(applicationName)
+				.secret("123456");
+	}
 }
-
 
 class BookmarkResource extends ResourceSupport {
 
-    private final Bookmark bookmark;
+	private final Bookmark bookmark;
 
-    public BookmarkResource(Bookmark bookmark) {
-        String username = bookmark.getAccount().getUsername();
-        this.bookmark = bookmark;
-        this.add(new Link(bookmark.uri, "bookmark-uri"));
-        this.add(linkTo(BookmarkRestController.class, username).withRel("bookmarks"));
-        this.add(linkTo(methodOn(BookmarkRestController.class, username).readBookmark(null, bookmark.getId())).withSelfRel());
-    }
+	public BookmarkResource(Bookmark bookmark) {
+		String username = bookmark.getAccount().getUsername();
+		this.bookmark = bookmark;
+		this.add(new Link(bookmark.uri, "bookmark-uri"));
+		this.add(linkTo(BookmarkRestController.class, username).withRel("bookmarks"));
+		this.add(linkTo(
+				methodOn(BookmarkRestController.class, username).readBookmark(null,
+						bookmark.getId())).withSelfRel());
+	}
 
-    public Bookmark getBookmark() {
-        return bookmark;
-    }
+	public Bookmark getBookmark() {
+		return bookmark;
+	}
 }
 
 @RestController
 @RequestMapping("/bookmarks")
 class BookmarkRestController {
 
-    private final BookmarkRepository bookmarkRepository;
+	private final BookmarkRepository bookmarkRepository;
 
-    private final AccountRepository accountRepository;
+	private final AccountRepository accountRepository;
 
-    @RequestMapping(method = RequestMethod.POST)
-    ResponseEntity<?> add(Principal principal, @RequestBody Bookmark input) {
-        String userId = principal.getName();
-        this.validateUser(userId);
+	@RequestMapping(method = RequestMethod.POST)
+	ResponseEntity<?> add(Principal principal, @RequestBody Bookmark input) {
+		String userId = principal.getName();
+		this.validateUser(userId);
 
-        return accountRepository.findByUsername(userId)
-                .map(account -> {
-                            Bookmark bookmark = bookmarkRepository.save(new Bookmark(account, input.uri, input.description));
+		return accountRepository
+				.findByUsername(userId)
+				.map(account -> {
+					Bookmark bookmark = bookmarkRepository.save(new Bookmark(account,
+							input.uri, input.description));
 
-                            HttpHeaders httpHeaders = new HttpHeaders();
+					HttpHeaders httpHeaders = new HttpHeaders();
 
-                            Link forOneBookmark = new BookmarkResource(bookmark).getLink("self");
-                            httpHeaders.setLocation(URI.create(forOneBookmark.getHref()));
+					Link forOneBookmark = new BookmarkResource(bookmark).getLink("self");
+					httpHeaders.setLocation(URI.create(forOneBookmark.getHref()));
 
-                            return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
-                        }
-                ).get();
-    }
+					return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+				}).get();
+	}
 
-    @RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
-    BookmarkResource readBookmark(Principal principal, @PathVariable Long bookmarkId) {
-        String userId = principal.getName();
-        this.validateUser(userId);
-        return new BookmarkResource(this.bookmarkRepository.findOne(bookmarkId));
-    }
+	@RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
+	BookmarkResource readBookmark(Principal principal, @PathVariable Long bookmarkId) {
+		String userId = principal.getName();
+		this.validateUser(userId);
+		return new BookmarkResource(this.bookmarkRepository.findOne(bookmarkId));
+	}
 
+	@RequestMapping(method = RequestMethod.GET)
+	Resources<BookmarkResource> readBookmarks(Principal principal) {
+		String userId = principal.getName();
+		this.validateUser(userId);
 
-    @RequestMapping(method = RequestMethod.GET)
-    Resources<BookmarkResource> readBookmarks(Principal principal) {
-        String userId = principal.getName();
-        this.validateUser(userId);
+		List<BookmarkResource> bookmarkResourceList = bookmarkRepository
+				.findByAccountUsername(userId).stream().map(BookmarkResource::new)
+				.collect(Collectors.toList());
+		return new Resources<BookmarkResource>(bookmarkResourceList);
+	}
 
-        List<BookmarkResource> bookmarkResourceList = bookmarkRepository.findByAccountUsername(userId)
-                .stream()
-                .map(BookmarkResource::new)
-                .collect(Collectors.toList());
-        return new Resources<BookmarkResource>(bookmarkResourceList);
-    }
+	@Autowired
+	BookmarkRestController(BookmarkRepository bookmarkRepository,
+			AccountRepository accountRepository) {
+		this.bookmarkRepository = bookmarkRepository;
+		this.accountRepository = accountRepository;
+	}
 
-    @Autowired
-    BookmarkRestController(BookmarkRepository bookmarkRepository,
-                           AccountRepository accountRepository) {
-        this.bookmarkRepository = bookmarkRepository;
-        this.accountRepository = accountRepository;
-    }
-
-    private void validateUser(String userId) {
-        this.accountRepository.findByUsername(userId)
-                .orElseThrow(() -> new UserNotFoundException(userId));
-    }
+	private void validateUser(String userId) {
+		this.accountRepository.findByUsername(userId).orElseThrow(
+				() -> new UserNotFoundException(userId));
+	}
 }
 
 @ControllerAdvice
 class BookmarkControllerAdvice {
 
-    @ResponseBody
-    @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    VndErrors userNotFoundExceptionHandler(UserNotFoundException ex) {
-        return new VndErrors("error", ex.getMessage());
-    }
+	@ResponseBody
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	VndErrors userNotFoundExceptionHandler(UserNotFoundException ex) {
+		return new VndErrors("error", ex.getMessage());
+	}
 }
 
-
+@SuppressWarnings("serial")
 class UserNotFoundException extends RuntimeException {
 
-    public UserNotFoundException(String userId) {
-        super("could not find user '" + userId + "'.");
-    }
+	public UserNotFoundException(String userId) {
+		super("could not find user '" + userId + "'.");
+	}
 }
