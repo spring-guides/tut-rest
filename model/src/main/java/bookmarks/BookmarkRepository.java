610e77e4562d7b2d20f2c9a3bf7aa57933525431
Uri Goldstein <urig@users.noreply.github.com>
2018-03-04 18:33:42 +0200

    Prevents "Invalid derived query! No property accountUsername found for type Bookmark" error in STS
    Based on https://stackoverflow.com/a/35129939/33404

--- model/src/main/java/bookmarks/BookmarkRepository.java
+++ model/src/main/java/bookmarks/BookmarkRepository.java
@@ -2,9 +2,11 @@ package bookmarks;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 
+@Query
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Collection<Bookmark> findByAccountUsername(String username);
 }
