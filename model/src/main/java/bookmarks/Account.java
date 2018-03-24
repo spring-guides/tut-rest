84f180171c5ca022b4af004d3820055a2047f8fc
Alter Ego <25475850+AlterEgoDev@users.noreply.github.com>
2018-03-24 19:35:47 +0100

    import fix

--- model/src/main/java/bookmarks/Account.java
+++ model/src/main/java/bookmarks/Account.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.HashSet;
