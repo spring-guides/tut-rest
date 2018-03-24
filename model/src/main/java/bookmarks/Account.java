105c9f03f153422a682b36e13111892f3eb915f7
Alter Ego <25475850+AlterEgoDev@users.noreply.github.com>
2018-03-24 19:31:54 +0100

    GenerationType.AUTO not working as expected
    use "@GeneratedValue(strategy = GenerationType.IDENTITY)" or "@GeneratedValue(strategy = GenerationType.TABLE)" instead of default "@GeneratedValue" with GenerationType.AUTO. GenerationType.AUTO creates primary keys over multiple tables - not the expected behavior. Example: inserting a row in empty table "a" generates id 1. Inserting a row in empty table "b" generates id 2.

--- model/src/main/java/bookmarks/Account.java
+++ model/src/main/java/bookmarks/Account.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Account {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
