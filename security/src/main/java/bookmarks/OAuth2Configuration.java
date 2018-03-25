14264981e1798ed431fc30f43ac7b0c3fb62150f
Alter Ego <25475850+AlterEgoDev@users.noreply.github.com>
2018-03-25 11:56:14 +0200

    fixed variable name

--- security/src/main/java/bookmarks/OAuth2Configuration.java
+++ security/src/main/java/bookmarks/OAuth2Configuration.java
@@ -61,11 +61,11 @@ class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
 		clients.inMemory()
-			.withClient("android-" + applicationName)
+			.withClient("android-" + APPLICATION_NAME)
 			.authorizedGrantTypes("password", "authorization_code", "refresh_token")
 			.authorities("ROLE_USER")
 			.scopes("write")
-			.resourceIds(applicationName)
+			.resourceIds(APPLICATION_NAME)
 			.secret("123456");
 	}
 }
