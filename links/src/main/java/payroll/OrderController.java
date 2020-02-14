268a844841951627aae975add63c3e7d34aa203e
ShayRomayo <noahking10@gmail.com>
2020-02-14 11:33:44 -0600

    Update to Spring HATEOAS 1.0+ Compatible
    Refactor
     - Resoure to EntityModel<>
     - Resources to CollectionModel<>
     - ResourceSupport to RepresentationModel
    Change the self-rel links to match the getId() protocol
    from HATEOAS 0.X release.
    
    [Issue: 84]

--- links/src/main/java/payroll/OrderController.java
+++ links/src/main/java/payroll/OrderController.java
@@ -1,14 +1,14 @@
 package payroll;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.ResourceSupport;
-import org.springframework.hateoas.Resources;
-import org.springframework.hateoas.VndErrors;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.mediatype.vnderrors.VndErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,54 +24,55 @@ import org.springframework.web.bind.annotation.RestController;
 class OrderController {
 
 	private final OrderRepository orderRepository;
-	private final OrderResourceAssembler assembler;
+	private final OrderModelAssembler assembler;
 
 	OrderController(OrderRepository orderRepository,
-					OrderResourceAssembler assembler) {
+                    OrderModelAssembler assembler) {
 
 		this.orderRepository = orderRepository;
 		this.assembler = assembler;
 	}
 
 	@GetMapping("/orders")
-	Resources<Resource<Order>> all() {
+	CollectionModel<EntityModel<Order>> all() {
 
-		List<Resource<Order>> orders = orderRepository.findAll().stream()
-			.map(assembler::toResource)
+		List<EntityModel<Order>> orders = orderRepository.findAll().stream()
+			.map(assembler::toModel)
 			.collect(Collectors.toList());
 
-		return new Resources<>(orders,
+		return new CollectionModel<>(orders,
 			linkTo(methodOn(OrderController.class).all()).withSelfRel());
 	}
 
 	@GetMapping("/orders/{id}")
-	Resource<Order> one(@PathVariable Long id) {
-		return assembler.toResource(
-			orderRepository.findById(id)
-				.orElseThrow(() -> new OrderNotFoundException(id)));
+	EntityModel<Order> one(@PathVariable Long id) {
+		Order order = orderRepository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException(id));
+
+		return assembler.toModel(order);
 	}
 
 	@PostMapping("/orders")
-	ResponseEntity<Resource<Order>> newOrder(@RequestBody Order order) {
+	ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
 
 		order.setStatus(Status.IN_PROGRESS);
 		Order newOrder = orderRepository.save(order);
 
 		return ResponseEntity
 			.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
-			.body(assembler.toResource(newOrder));
+			.body(assembler.toModel(newOrder));
 	}
 	// end::main[]
 	
 	// tag::delete[]
 	@DeleteMapping("/orders/{id}/cancel")
-	ResponseEntity<ResourceSupport> cancel(@PathVariable Long id) {
+	ResponseEntity<RepresentationModel> cancel(@PathVariable Long id) {
 
 		Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
 
 		if (order.getStatus() == Status.IN_PROGRESS) {
 			order.setStatus(Status.CANCELLED);
-			return ResponseEntity.ok(assembler.toResource(orderRepository.save(order)));
+			return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
 		}
 
 		return ResponseEntity
@@ -82,13 +83,13 @@ class OrderController {
 
 	// tag::complete[]
 	@PutMapping("/orders/{id}/complete")
-	ResponseEntity<ResourceSupport> complete(@PathVariable Long id) {
+	ResponseEntity<RepresentationModel> complete(@PathVariable Long id) {
 		
 			Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
 
 			if (order.getStatus() == Status.IN_PROGRESS) {
 				order.setStatus(Status.COMPLETED);
-				return ResponseEntity.ok(assembler.toResource(orderRepository.save(order)));
+				return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
 			}
 
 			return ResponseEntity
