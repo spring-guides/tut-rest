860a94bd7a6d868421ab7ccaf522400258a2a833
ShayRomayo <noahking10@gmail.com>
2020-02-17 10:36:54 -0600

    Add untracked files and maven wrapper

---
+++ links/src/main/java/payroll/EmployeeModelAssembler.java
@@ -0,0 +1,18 @@
+package payroll;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+	@Override
+	public EntityModel<Employee> toModel(Employee employee) {
+		return new EntityModel<>(employee,
+			linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+			linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	}
+}
---
+++ links/src/main/java/payroll/OrderModelAssembler.java
@@ -0,0 +1,35 @@
+package payroll;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
+
+	@Override
+	public EntityModel<Order> toModel(Order order) {
+
+		// Unconditional links to single-item resource and aggregate root
+
+		EntityModel<Order> orderModel = new EntityModel<>(order,
+			linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
+			linkTo(methodOn(OrderController.class).all()).withRel("orders")
+		);
+
+		// Conditional links based on state of the order
+		
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			orderModel.add(
+				linkTo(methodOn(OrderController.class)
+					.cancel(order.getId())).withRel("cancel"));
+			orderModel.add(
+				linkTo(methodOn(OrderController.class)
+					.complete(order.getId())).withRel("complete"));
+		}
+
+		return orderModel;
+	}
+}
