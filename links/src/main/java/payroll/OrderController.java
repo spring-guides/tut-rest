80a51e8ad60892fb661dd8099ca4f161f5a4eea3
Greg L. Turnquist <gturnquist@vmware.com>
2020-05-27 13:38:09 -0500

    Polish HTTP Problem usage.

--- links/src/main/java/payroll/OrderController.java
+++ links/src/main/java/payroll/OrderController.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -77,6 +79,7 @@ class OrderController {
 
 		return ResponseEntity //
 				.status(HttpStatus.METHOD_NOT_ALLOWED) //
+				.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
 				.body(Problem.create() //
 						.withTitle("Method not allowed") //
 						.withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
@@ -97,6 +100,7 @@ class OrderController {
 
 		return ResponseEntity //
 				.status(HttpStatus.METHOD_NOT_ALLOWED) //
+				.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
 				.body(Problem.create() //
 						.withTitle("Method not allowed") //
 						.withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
