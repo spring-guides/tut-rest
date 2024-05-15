6c0b4bd660308f4bfe52de5065cf88ba014e037d
Robert McNees <86265089+robertmcnees@users.noreply.github.com>
2024-05-15 06:27:11 -0400

    Tutorial update (#127)
    * Updated README, Boot version, and maven wrapper

--- links/src/main/java/payroll/Order.java
+++ links/src/main/java/payroll/Order.java
@@ -2,10 +2,10 @@ package payroll;
 
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "CUSTOMER_ORDER")
