f16f14656c8635612aa273d93d3ec917d0a7db27
Alex Korpis <alexkorpis@live.com>
2024-06-07 12:15:32 +0200

    Remove method call without effect (#128)
    * Remove method call without effect
    
    Since id is @GeneratedValue setId() wouldn't do anything

--- links/src/main/java/payroll/EmployeeController.java
+++ links/src/main/java/payroll/EmployeeController.java
@@ -76,7 +76,6 @@ class EmployeeController {
 					return repository.save(employee);
 				}) //
 				.orElseGet(() -> {
-					newEmployee.setId(id);
 					return repository.save(newEmployee);
 				});
 
